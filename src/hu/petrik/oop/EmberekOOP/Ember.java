17afea76393606ac1c814aed82304755f675eadd
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-15 09:34:37 +0200

    Add variables and constructor to Ember class

--- src/hu/petrik/oop/EmberekOOP/Ember.java
+++ src/hu/petrik/oop/EmberekOOP/Ember.java
@@ -1,2 +1,15 @@
-package hu.petrik.oop.EmberekOOP;public class Ember {
+package hu.petrik.oop.EmberekOOP;
+
+public class Ember {
+    private String name;
+    private String birthDate;
+    private String birthPlace;
+
+    public Ember(String name, String birthDate, String birthPlace) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.birthPlace = birthPlace;
+    }
+
+
 }
