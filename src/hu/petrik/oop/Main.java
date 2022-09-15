e256ead5ba4f6a856956371403a9305719f74499
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-15 09:12:16 +0200

    Add test for pont class objects

--- src/hu/petrik/oop/Main.java
+++ src/hu/petrik/oop/Main.java
@@ -1,7 +1,17 @@
 package hu.petrik.oop;
 
+import hu.petrik.oop.pont.pont;
+
 public class Main {
     public static void main(String[] args) {
+        pont p1 = new pont();
+        pont p2 = new pont(0,2);
+        pont p3 = new pont(3,5);
 
+        System.out.println(p1.valuesToString());
+        System.out.println(p2.valuesToString());
+        System.out.println(p3.valuesToString());
+        System.out.println(p2.distanceFromZero());
+        System.out.println(p2.distanceToAnotherPoint(p3));
     }
 }
